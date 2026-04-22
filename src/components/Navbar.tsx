7821d51b151417f365289e6af8e7b3f87675ad36
Aniket-Vyass <aniketvyas05@gmail.com>
2026-04-22 21:14:18 +0530

    changed info

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,7 +43,7 @@ const Navbar = () => {
     <>
       <div className="header">
         <a href="/#" className="navbar-title" data-cursor="disable">
-          Logo
+          Harsh Creatives
         </a>
         <a
           href="mailto:adityaharsh52@gmail.com"
