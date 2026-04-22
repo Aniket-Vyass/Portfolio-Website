3edefece650f2b329e6d3c268d877c26717414d4
Aniket-Vyass <aniketvyas05@gmail.com>
2026-04-22 20:48:19 +0530

    email fixed

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -62,8 +62,8 @@ const Loading = ({ percent }: { percent: number }) => {
       <div className="loading-screen">
         <div className="loading-marquee">
           <Marquee>
-            <span> A Creative Developer</span> <span>A Creative Designer</span>
-            <span> A Creative Developer</span> <span>A Creative Designer</span>
+            <span> A Creative Video Editor</span> <span>Motion Graphics Expert</span>
+            <span> A Creative Video Editor</span> <span>Motion Graphics Expert</span>
           </Marquee>
         </div>
         <div
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -46,11 +46,11 @@ const Navbar = () => {
           Logo
         </a>
         <a
-          href="mailto:example@mail.com"
+          href="mailto:adityaharsh52@gmail.com"
           className="navbar-connect"
           data-cursor="disable"
         >
-          example@mail.com
+          adityaharsh52@gmail.com
         </a>
         <ul>
           <li>
